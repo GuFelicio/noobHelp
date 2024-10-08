d05d0417dccd24c6bcec2991ac6b68371931d22e
Gustavo Felicio <gu.felicio10@gmail.com>
2024-10-08 13:46:16 -0300

    Exclusão da pasta de testes com o Streamlit!

--- venv/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Carregar os dados
-data_path = r"F:\Devs\helpNBlol\match_details_transformed.csv"
-data = pd.read_csv(data_path)
-
-# Exibir todas as colunas disponíveis no DataFrame
-print("Colunas disponíveis:", data.columns.tolist())
